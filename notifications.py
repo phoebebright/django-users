fba1b04911b78dafa9e3e94a72c41261ea3563b0
phoebebright <phoebebright310@gmail.com>
2024-12-06 12:52:57 +0000

    initial module

---
+++ notifications.py
@@ -0,0 +1,17 @@
+from django.core.mail import mail_admins
+
+from config import settings
+from tools.decorators import notifications_on
+
+
+@notifications_on
+def on_new_user_unverified(instance, message, request=None, user=None):
+    from users.models import CustomUser
+
+    mail_admins(f"User signing up {instance} for {settings.SITE_NAME}", f"email: {instance.email}, name: {instance.full_name}", fail_silently=True)
+
+@notifications_on
+def on_new_user_verified(instance, message, request=None, user=None):
+    from users.models import CustomUser
+
+    mail_admins(f"User verified {instance} for {settings.SITE_NAME}", f"email: {instance.email}, name: {instance.full_name}", fail_silently=True)
