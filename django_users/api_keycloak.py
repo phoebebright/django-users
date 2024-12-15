ef6372a07b2f455d15e76bbca2a3dcb47d424fa2
phoebebright <phoebebright310@gmail.com>
2024-12-15 19:28:48 +0000

    api_keycloak

--- django_users/api_keycloak.py
+++ django_users/api_keycloak.py
@@ -1,26 +1,28 @@
+from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
-from rest_framework import filters, generics, mixins, status, viewsets
+from rest_framework import status, viewsets
 
 from rest_framework.response import Response
-
-
-
-from .serializers import *
-
-
-
 
 class SyncUser(viewsets.ModelViewSet):
     '''external client sending info on a new user or with updated info about a user.
     return whether user created or not
     Will be authenticating '''
 
-    queryset = CustomUser.objects.none()
-    serializer_class = UserSyncSerializer
 
     http_method_names = ['post', ]
 
+    def get_queryset(self):
+        User = get_user_model()
+        return User.objects.none()
+
+    def get_serializer_class(self):
+        try:
+            from users.serializers import UserSyncSerializer  # Dynamically import the serializer
+            return UserSyncSerializer
+        except ImportError:
+            raise NotImplementedError("UserSyncSerializer must be implemented in the project's `users.serializers` module")
 
     def create(self, request, *args, **kwargs):
 
@@ -28,7 +30,8 @@ class SyncUser(viewsets.ModelViewSet):
         # removed code that created or updated a user from post data on 20Jun20
 
         if request.user and not request.user.is_anonymous:
-            response_data = UserSerializer(request.user).data
+            serializer = self.get_serializer(data=request.data)
+            response_data = serializer(request.user).data
         else:
 
             serializer = self.get_serializer(data=request.data)
