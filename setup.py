0ac91d328154a79c108db41003cf96a173ceb34b
phoebebright <phoebebright310@gmail.com>
2024-12-15 16:40:30 +0000

    turning into external app

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-users',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
