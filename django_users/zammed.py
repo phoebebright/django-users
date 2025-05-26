3b3fb7bb5f100fce6430ade6498f17bdf0a0330b
phoebebright <phoebebright310@gmail.com>
2025-05-26 20:56:09 +0100

    adding zammed integration

--- django_users/zammed.py
+++ django_users/zammed.py
@@ -5,24 +5,26 @@ from django.conf import settings
 # Note the Host URL should be in this format: 'https://zammad.example.org/api/v1/'
 client = ZammadAPI(url=settings.ZAMMED['host'], username=settings.ZAMMED['username'], password=settings.ZAMMED['pw'])
 
-# Example: Access all users
-this_page = client.user.all()
-for user in this_page:
-    print(user)
+#
+# # Example: Access all users
+# this_page = client.user.all()
+# for user in this_page:
+#     print(user)
+#
+# # Example: Get information about the current user
+# print(client.user.me())
 
-# Example: Get information about the current user
-print(client.user.me())
-
-# Example: Create a ticket
-params = {
-   "title": "Help me!",
-   "group": "2nd Level",
-   "customer": "david@example.com",
-   "article": {
-      "subject": "My subject",
-      "body": "I am a message!",
-      "type": "note",
-      "internal": false
+def add_ticket(request):
+   # Example: Create a ticket
+   params = {
+      "title": "Help me!",
+      "group": "2nd Level",
+      "customer": "david@example.com",
+      "article": {
+         "subject": "My subject",
+         "body": "I am a message!",
+         "type": "note",
+         "internal": False
+      }
    }
-}
-new_ticket = client.ticket.create(params=params)
+   new_ticket = client.ticket.create(params=params)
