2b9ec39df978cc835359841ad01c8e57142a5b86
phoebebright <phoebebright310@gmail.com>
2025-11-19 13:50:51 +0000

    fix confirm

--- django_users/forms.py
+++ django_users/forms.py
@@ -469,6 +469,10 @@ class PersonForm(forms.ModelForm):
         model = apps.get_model('users', 'Person')
         fields = ['formal_name', 'friendly_name', 'sortable_name']
 
+#TODO: need to reimplement
+class HelpDeskTicket:
+    pass
+
 
 class SupportTicketForm(forms.ModelForm):
 
